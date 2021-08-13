d1b34024a0fc5a248fb41471235dc85da26ea610
Manuel Ortega <74545902+ManuelOrtegaSSU@users.noreply.github.com>
2021-08-13 11:46:30 +0200

    Implement Roman number test

--- roman-to-decimal/roman-to-decimal.js
+++ roman-to-decimal/roman-to-decimal.js
@@ -6,7 +6,19 @@ function romanToDecimal(numeral) {
         return 9;
     }
 
-    return 5;
+
+        
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    
+    return romanMap[numeral];
 }
 
 module.exports = romanToDecimal;
