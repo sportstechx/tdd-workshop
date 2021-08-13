043107d46a87880798fcf9ccae7fe99831840d6f
JS <jsanchezcriado@gmail.com>
2021-08-13 11:53:40 +0200

    Reorder tests

--- roman-to-decimal/roman-to-decimal.test.js
+++ roman-to-decimal/roman-to-decimal.test.js
@@ -1,11 +1,11 @@
 const romanToDecimal = require('./roman-to-decimal');
 
-test('V is 5 in decimal', () => {
-  expect(romanToDecimal('V')).toBe(5);
-});
-
 test('I is 1 in decimal', () => {
   expect(romanToDecimal('I')).toBe(1);
+});
+
+test('V is 5 in decimal', () => {
+  expect(romanToDecimal('V')).toBe(5);
 });
 
 test('X is 10 in decimal', () => {
