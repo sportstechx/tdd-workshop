aa6ea2e30f540c95607ca2adadd3c3d076428755
JS <jsanchezcriado@gmail.com>
2021-08-13 11:39:23 +0200

    Set I test green

--- roman-to-decimal/roman-to-decimal.js
+++ roman-to-decimal/roman-to-decimal.js
@@ -1,7 +1,10 @@
 function romanToDecimal(numeral) {
     // Write your implementation here
 
-    return 5
+    if (numeral == "I") {
+        return 1
+    }
+    return 5    
 }
 
 module.exports = romanToDecimal;
