32256f9d46e4403284164a1663ba65fe5cbb93fc
Sergio Marina <s.marina@signa-sportsunited.com>
2021-08-13 11:23:28 +0200

    Add VII test

--- roman-to-decimal/roman-to-decimal.test.js
+++ roman-to-decimal/roman-to-decimal.test.js
@@ -3,3 +3,7 @@ const romanToDecimal = require('./roman-to-decimal');
 test('V is 5 in decimal', () => {
   expect(romanToDecimal('V')).toBe(5);
 });
+
+test('VII is 7 in decimal', () => {
+  expect(romanToDecimal('VII')).toBe(7);
+});
