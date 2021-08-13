4dbd0a8715d77311220cbcd60f5b146365e92ceb
JS <jsanchezcriado@gmail.com>
2021-08-13 11:29:34 +0200

    Add red test for the 'I'

--- roman-to-decimal/roman-to-decimal.test.js
+++ roman-to-decimal/roman-to-decimal.test.js
@@ -3,3 +3,8 @@ const romanToDecimal = require('./roman-to-decimal');
 test('V is 5 in decimal', () => {
   expect(romanToDecimal('V')).toBe(5);
 });
+
+test('I is 1 in decimal', () => {
+  expect(romanToDecimal('I')).toBe(1);
+});
+
