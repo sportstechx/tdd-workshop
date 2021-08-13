d54bdfd5fb1942d5e9c6d7f250746a2817b94c21
JS <jsanchezcriado@gmail.com>
2021-08-13 12:06:39 +0200

    Refactor

--- roman-to-decimal/roman-to-decimal.js
+++ roman-to-decimal/roman-to-decimal.js
@@ -1,29 +1,30 @@
 function romanToDecimal(numeral) {
     // Write your implementation here
 
-    if (numeral == "II") {
-        return 2
-    }
-
-    if (numeral == "I") {
+    if (numeral === "I") {
         return 1
     }
-    else if (numeral == "X") {
+    if (numeral === "II") {
+        return 2
+    }
+    if (numeral === "V") {
+        return 5
+    }
+    if (numeral === "X") {
         return 10
     }
-    else if (numeral == "L") {
+    if (numeral === "L") {
         return 50
     }
-    else if (numeral == "C") {
+    if (numeral === "C") {
         return 100
     }
-    else if (numeral == "D") {
+    if (numeral === "D") {
         return 500
     }
-    else if (numeral == "M") {
+    if (numeral === "M") {
         return 1000
     }
-    return 5    
 }
 
 module.exports = romanToDecimal;
