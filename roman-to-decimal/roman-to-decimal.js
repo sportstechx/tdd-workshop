a337f20fb92e5f1d6a49f461e7704cee9d962a5e
Manuel García <mgarciadelojo@gmail.com>
2021-08-13 12:07:18 +0200

    Temp, solution to calculate substracted numbers

--- roman-to-decimal/roman-to-decimal.js
+++ roman-to-decimal/roman-to-decimal.js
@@ -1,11 +1,37 @@
 function romanToDecimal(numeral) {
     const dict = {
+        'IV': 4,
         'I': 1,
         'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
         'M': 1000,
     }
-    return numeral.split('')
-        .reduce((acc, current) => acc + dict[current], 0);
+
+    let result = 0;
+
+    while (numeral.length > 0) {
+        Object.keys(dict).forEach((value) => {
+            console.log(numeral)
+            if (numeral.startsWith(value)) {
+                result += dict[value]
+                numeral = numeral.slice(value.length)
+            }
+        })
+    }
+
+    // // LCIX
+
+    // const acc = {
+    //     result: 0,
+    //     roman: numeral,
+    // }
+
+    // return numeral.reduce((_acc, current) => {
+
+    // }, acc);
 }
 
 module.exports = romanToDecimal;
